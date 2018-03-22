9887a21dd6ca4477410466ccfaa23cc907dab5f3
parkersalterego <parkersalterego92@gmail.com>
2018-03-21 19:41:54 -0700

    week-09-b in class

--- Applications/Week-09/03-HttpsExpressBasic/webapp/app.js
+++ Applications/Week-09/03-HttpsExpressBasic/webapp/app.js
@@ -9,7 +9,7 @@ var index = require( './routes/index' );
 
 require( 'dotenv' )
   .config();
-
+ 
 var app = express();
 
 // view engine setup
@@ -24,7 +24,7 @@ app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( cookieParser() );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
 
-app.use( '/', index );
+app.use( '/', index ); 
 
 // catch 404 and forward to error handler
 app.use( function( req, res, next ) {
